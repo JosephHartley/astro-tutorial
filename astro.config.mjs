1a68ad88a9de8a5f034db4c390372c44667729a0
Joe Hartley <joejohnhartley@gmail.com>
2024-03-05 21:22:15 +0000

    Adds greeting component and dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://master--stupendous-cheesecake-49c799.netlify.app"
+  site: "https://master--stupendous-cheesecake-49c799.netlify.app",
+  integrations: [preact()]
 });
